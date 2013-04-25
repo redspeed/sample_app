d1056960234dfbfcbd5acecf5c7fd08fc31b9f7e
Glen Manangh <gjman@clear.net.nz>
2013-04-25 15:32:19 +1200

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,12 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
 
+  def home
+    if signed_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
+  end
+  
   def help
   end
 
@@ -9,5 +14,5 @@ class StaticPagesController < ApplicationController
   end
 
   def contact
-  end  
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,27 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
-  before_filter :correct_user,   only: [:edit, :update]
-  before_filter :admin_user,     only: :destroy
+  before_filter :signed_in_user,
+                only: [:index, :edit, :update, :destroy, :following, :followers]
+  before_filter :correct_user, only: [:edit, :update]
+  before_filter :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
-  	@user = User.find(params[:id])
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
-  	@user = User.new
+    @user = User.new
   end
 
   def create
     @user = User.new(params[:user])
     if @user.save
-    	flash[:success] = "Welcome to  Rate the Plate!"
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
@@ -22,7 +29,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
@@ -35,31 +41,34 @@ class UsersController < ApplicationController
     end
   end
 
-  def index
-    @users = User.paginate(page: params[:page])
-  end
-
   def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User destroyed."
     redirect_to users_url
   end
 
-  private
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  private
 
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_path) unless current_user?(@user)
+      redirect_to(root_url) unless current_user?(@user)
     end
 
-     def admin_user
-      redirect_to(root_path) unless current_user.admin?
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 end
